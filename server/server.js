7c01782ded7483792b795a2648196f1903d97607
Atlas <jack@bentsen.us>
2021-08-27 11:07:28 -0700

    fresh start - want to fix file structure

--- server/server.js
+++ server/server.js
@@ -1,37 +1,21 @@
 const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
-const { ApolloServerPluginDrainHttpServer } = require('apollo-server-core');
 const path = require('path');
-const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+const app = express();
 const PORT = process.env.PORT || 3001;
 
-async function startApolloServer() {
-  const app = express();
-  // const httpServer = http.createServer(app);
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    // plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
-  });
-  await server.start();
-  server.applyMiddleware({ app });
-  app.use(express.urlencoded({ extended: true }));
-  app.use(express.json());
-  // if we're in production, serve client/build as static assets
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
-  }
-  
-  app.use(routes);
-  
-  db.once('open', () => {
-    app.listen(PORT, () => {
-      console.log(`🌍 Now listening on http://localhost:${PORT}`);
-      console.log(`GraphQl Path is http://localhost:${PORT}${server.graphqlPath}`);
-    });
-  });
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
 }
-startApolloServer();
+
+app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+});
